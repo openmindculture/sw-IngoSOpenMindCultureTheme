62d6820cbba54acf86bee976e377363a8b8a1e98
Ingo Steinke <ingosteinke@ingosteinke.de>
2026-03-12 18:37:22 +0100

    cart and checkout style

---
+++ src/IngoSOpenMindCultureTheme/src/Resources/app/storefront/src/country-state-select/country-state-select.plugin.js
@@ -0,0 +1,15 @@
+import Plugin from 'src/plugin-system/plugin.class';
+// disable core plugin that adds state country (Bundesland)
+
+export default class CountryStateSelectPlugin extends Plugin {
+    init() {
+    }
+    initSelects() {
+    }
+
+    _updateCountryStateSelect() {
+    }
+
+    _onCountryChange() {
+    }
+}
--- src/IngoSOpenMindCultureTheme/src/Resources/app/storefront/src/main.js
+++ src/IngoSOpenMindCultureTheme/src/Resources/app/storefront/src/main.js
@@ -1,5 +1,14 @@
 import IngoSOpenMindCultureTheme from './ingos-open-mind-culture-theme/ingos-open-mind-culture-theme';
+import CountryStateSelectPlugin from './country-state-select/country-state-select.plugin';
+
 // Import other plugins if necessary
 // Register your plugin via the existing PluginManager
 const PluginManager = window.PluginManager;
 PluginManager.register('IngoSOpenMindCultureTheme', IngoSOpenMindCultureTheme);
+
+// unregister or replace core plugins
+window.PluginManager.override(
+    'CountryStateSelect',
+    CountryStateSelectPlugin,
+    '[data-country-state-select]'
+);
